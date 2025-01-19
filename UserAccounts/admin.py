6b4e37d02dc8c838b1fd97a9b3e9dc45071b195e
fmcmkdict <ict.unit@fmcmakurdi.gov.ng>
2025-01-19 04:53:01 +0100

    Updated configurations

--- UserAccounts/admin.py
+++ UserAccounts/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+# import these because of custom user
+
+
+from django.contrib.auth.admin import UserAdmin
+from .models import UserAccounts
+
+# uncomment after checking and editing
+class CustomUserAdmin(UserAdmin):
+    
+    model = UserAccounts
+    list_display = ['username']
+    
+    # fieldsets = (
+    #     (None, {
+    #         'fields': ('username', 'password')
+    #     }),
+    #     ('Personal info', {
+    #         'fields': ('sur_name','first_name','other_name','dob','gender','phone','class_admitted','session_admitted','admission_number')
+    #     }),
+    #     ('Permissions', {
+    #         'fields': (
+    #             'is_active', 'is_staff','is_superuser',
+    #             'groups', 'user_permissions'
+    #             )
+    #     }),
+    #     ('Important dates', {
+    #         'fields': ('last_login', 'date_joined')
+    #     }),
+    #     ('Additional info', {
+    #         'fields': ('is_student', 'is_teacher',)
+    #     })
+    # )
+
+    
+    
+
+
+ 
+admin.site.register(UserAccounts,CustomUserAdmin)
--- UserAccounts/models.py
+++ UserAccounts/models.py
@@ -1,9 +1,6 @@
 from django.db import models
-
-
 from django.utils import timezone
 from datetime import date
-
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
 class CustomUserManager(BaseUserManager):
